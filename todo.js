34551f109d4abaaacc00378686d4068bc743988b
khlim <yorsild@naver.com>
2019-08-28 23:29:50 +0900

    make index.html

---
+++ todo.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+
+	document.getElementById('btn-add').onclick = function(){
+
+		var inputTodo = document.getElementById('input-todo');
+
+		var todoList = document.getElementById('todo-list');
+
+		var item = document.createElement('li');
+
+		item.innerHTML  = inputTodo.value;
+
+		item.addEventListener('click', function () {
+				
+			// console.log(this);
+			todoList.removeChild(this);	
+
+		});
+
+		todoList.appendChild(item);
+
+		inputTodo.value = '';
+	}
+}
